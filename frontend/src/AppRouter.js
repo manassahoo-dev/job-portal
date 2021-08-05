4eb6e8e2b98143cc423b2f77e1bf9def049a5e3c
kaveri10 <kaverithombare1999@gmail.com>
2021-08-05 23:21:52 +0530

    sign up login page

--- frontend/src/AppRouter.js
+++ frontend/src/AppRouter.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import TopNavigationBar from './components/TopNavigationBar';
 import Login from './pages/Login';
+import SignUp from "./pages/SignUp";
 
 function AppRouter() {
   return (
@@ -9,6 +10,9 @@ function AppRouter() {
         <TopNavigationBar />
         <Switch>
           <Route exact path='/login' component={Login} />
+        </Switch>
+        <Switch>
+          <Route exact path='/sign-up' component={SignUp} />
         </Switch>
       </BrowserRouter>
     </div>
--- frontend/src/components/TopNavigationBar.js
+++ frontend/src/components/TopNavigationBar.js
@@ -1,21 +1,32 @@
-import { AppstoreOutlined, MailOutlined } from '@ant-design/icons';
+import { AppstoreOutlined, HomeOutlined } from '@ant-design/icons';
 import { Menu } from 'antd';
-
+import Logo from "../assets/logo.png"
 function TopNavigationBar() {
     const handleClick = e => {
         console.log('click ', e);
     };
     return (
         <Menu onClick={handleClick} mode="horizontal">
-            <Menu.Item key="mail" icon={<MailOutlined />}>
-                Navigation One
+            <Menu.Item>
+                <img
+                    src={Logo}
+                    height='30'
+                    className="d-inline-block align-top"
+                    filter="invert(1)"
+                    alt="React Bootstrap logo"
+                />
+            </Menu.Item>
+            <Menu.Item key="mail" icon={<HomeOutlined />}>
+                <a href="/login">
+                    Home
+                </a>
             </Menu.Item>
             <Menu.Item key="app" disabled icon={<AppstoreOutlined />}>
-                Navigation Two
+                About Us
             </Menu.Item>
             <Menu.Item key="alipay">
-                <a href="https://ant.design" target="_blank" rel="noopener noreferrer">
-                    Navigation Four - Link
+                <a href="/sign-up">
+                    Sign Up
                 </a>
             </Menu.Item>
         </Menu>
