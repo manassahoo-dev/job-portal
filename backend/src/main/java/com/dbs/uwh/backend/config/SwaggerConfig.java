df648b3f7d2c75b860be479d3d5176c93b5deb07
Manas Ranjan Sahoo <manas_sahoo@outlook.com>
2021-08-08 23:41:53 +0530

    Swagger Config Documentation

--- backend/src/main/java/com/dbs/uwh/backend/config/SwaggerConfig.java
+++ backend/src/main/java/com/dbs/uwh/backend/config/SwaggerConfig.java
@@ -1,17 +1,19 @@
 package com.dbs.uwh.backend.config;
 
+import static com.google.common.base.Predicates.or;
+import static springfox.documentation.builders.PathSelectors.regex;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.google.common.base.Predicate;
 
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-import static springfox.documentation.builders.PathSelectors.regex;
-import static com.google.common.base.Predicates.or;
 
 @Configuration
 @EnableSwagger2
@@ -19,12 +21,8 @@ public class SwaggerConfig {
 
 	@Bean
 	public Docket postsApi() {
-		return new Docket(DocumentationType.SWAGGER_2).groupName("public-api").apiInfo(apiInfo()).select()
-				.paths(postPaths()).build();
-	}
-
-	private Predicate<String> postPaths() {
-		return or(regex("/users.*"), regex("/companies.*"));
+		return new Docket(DocumentationType.SWAGGER_2).groupName("api").apiInfo(apiInfo()).select()
+				.paths(PathSelectors.any()).build();
 	}
 
 	private ApiInfo apiInfo() {
