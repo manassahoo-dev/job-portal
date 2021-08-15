0e90e6bfa843c682b9d63e35f2adf27359b7a59b
vivaswanth vidya katravulapalli <vivaswanth.k@gmail.com>
2021-08-15 23:26:43 +0530

    Minor change to add entity annotation to course entity

--- backend/src/main/java/com/dbs/uwh/backend/model/Course.java
+++ backend/src/main/java/com/dbs/uwh/backend/model/Course.java
@@ -1,7 +1,13 @@
 package com.dbs.uwh.backend.model;
 
+import javax.persistence.Entity;
 import com.sun.istack.NotNull;
+import lombok.Getter;
+import lombok.Setter;
 
+@Entity
+@Getter
+@Setter
 public class Course extends BaseEntity {
 
 	private static final long serialVersionUID = 6082391690556986461L;
