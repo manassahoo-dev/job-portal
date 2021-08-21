d1ca9e49aa98f6269e46d0e2f7ca50730eeebe47
Manas Sahoo <manas_sahoo@outlook.com>
2021-08-21 16:54:19 +0530

    Code refactor Context

--- frontend/src/components/quiz/index.js
+++ frontend/src/components/quiz/index.js
@@ -1,17 +1,17 @@
 import { Col, List } from "antd";
 import React, { useContext } from "react";
-import QuizContext from "../../contexts/QuizContext";
+import AppContext from "../../contexts/AppContext";
 import api from "../../services/api";
 import ApiRequest from "../../services/ApiRequest";
 import QuizCard from "./Card";
 
 function QuizList() {
-    const { quizData } = useContext(QuizContext);
-    const { data, error, loading } = ApiRequest('GET', `${api.QUIZ}/category/${quizData.categoryId}?quizType=${quizData.quizType}`, quizData);
+    const { contextData } = useContext(AppContext);
+    const { data, error, loading } = ApiRequest('GET', `${api.QUIZ}/category/${contextData.categoryId}?quizType=${contextData.quizType}`, contextData);
 
     return (
         <>
-            {quizData.isAddEdit ? <Col span={12}>
+            {contextData.isAddEdit ? <Col span={12}>
                 {data.map((quiz, index) => <Col xs={24} key={index}><QuizCard quiz={quiz} /></Col>)}
             </Col>
                 :
