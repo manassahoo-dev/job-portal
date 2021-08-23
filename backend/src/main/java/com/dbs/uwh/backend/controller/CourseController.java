e48f20234255d7977b0ae283f4986ed33f911530
Manas Sahoo <manas_sahoo@outlook.com>
2021-08-23 20:41:18 +0530

    Course API added

--- backend/src/main/java/com/dbs/uwh/backend/controller/CourseController.java
+++ backend/src/main/java/com/dbs/uwh/backend/controller/CourseController.java
@@ -2,10 +2,17 @@ package com.dbs.uwh.backend.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dbs.uwh.backend.model.Course;
@@ -24,5 +31,18 @@ public class CourseController extends GenericRestController<Course, Long> {
 	@GetMapping(value = "/category/{id}")
 	public List<Course> findByCategoryId(@PathVariable("id") Long id) {
 		return courseService.findByCategoryId(id);
+	}
+	
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Course create(@RequestBody @Valid Course course) {
+		return courseService.create(course);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	@ResponseStatus(HttpStatus.OK)
+	public void delete(@PathVariable("id") Long id) {
+		courseService.deleteById(id);
 	}
 }
