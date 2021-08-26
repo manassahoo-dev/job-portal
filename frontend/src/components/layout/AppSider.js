e84c8de850401546bbc60919b3bb8d08bcbc7156
Manas Sahoo <manas_sahoo@outlook.com>
2021-08-27 01:34:20 +0530

    Icons Updated

--- frontend/src/components/layout/AppSider.js
+++ frontend/src/components/layout/AppSider.js
@@ -1,15 +1,9 @@
 import { Layout, Menu } from 'antd';
 import React, { useState } from 'react';
-import { AiFillHome } from "react-icons/ai";
-import { BiBookReader } from "react-icons/bi";
-import { BsPersonFill } from "react-icons/bs";
-import { FaGraduationCap } from "react-icons/fa";
-import { GiBookPile, GiSuitcase, GiTeacher } from 'react-icons/gi';
-import { ImPen } from "react-icons/im";
-import { IoIosBookmarks } from "react-icons/io";
-import { IoSettingsSharp } from "react-icons/io5";
-import { RiHandHeartFill } from "react-icons/ri";
-import { TiGroup } from "react-icons/ti";
+import {
+    FiBarChart2, FiBookOpen, FiBriefcase, FiCheckSquare, FiEdit3,
+    FiFileText, FiHome, FiList, FiSend, FiSettings, FiTerminal, FiUser
+} from "react-icons/fi";
 import { Link } from 'react-router-dom';
 const { Sider } = Layout;
 
@@ -18,18 +12,18 @@ const AppLayout = () => {
     const [collapsed, setCollapsed] = useState(false);
 
     const menuItems = [
-        { name: 'Home', icon: <AiFillHome />, path: '/admin/dashboard' },
-        { name: 'Student', icon: <BsPersonFill />, path: '/admin/students' },
-        { name: 'Batch', icon: <GiTeacher />, path: '/admin/batches' },
-        { name: 'Course', icon: <GiBookPile />, path: '/admin/courses' },
-        { name: 'Skill Test', icon: <BiBookReader />, path: '/admin/skills' },
-        { name: 'Counselling', icon: <TiGroup />, path: '/admin/counselling' },
-        { name: 'Aptitude Test', icon: <IoIosBookmarks />, path: '/admin/aptitudes' },
-        { name: 'Enquiry', icon: <RiHandHeartFill />, path: '/admin/enquiry' },
-        { name: 'Exam', icon: <ImPen />, path: '/admin/exams' },
-        { name: 'Job', icon: <GiSuitcase />, path: '/admin/jobs' },
-        { name: 'Report', icon: <FaGraduationCap />, path: '/admin/reports' },
-        { name: 'Settings', icon: <IoSettingsSharp />, path: '/admin/settings' },
+        { name: 'Home', icon: <FiHome />, path: '/admin/dashboard' },
+        { name: 'Student', icon: <FiUser />, path: '/admin/students' },
+        { name: 'Batch', icon: <FiBarChart2 />, path: '/admin/batches' },
+        { name: 'Course', icon: <FiBookOpen />, path: '/admin/courses' },
+        { name: 'Skill Test', icon: <FiTerminal />, path: '/admin/skills' },
+        { name: 'Counselling', icon: <FiList />, path: '/admin/counselling' },
+        { name: 'Aptitude Test', icon: <FiCheckSquare />, path: '/admin/aptitudes' },
+        { name: 'Enquiry', icon: <FiSend />, path: '/admin/enquiry' },
+        { name: 'Exam', icon: <FiEdit3 />, path: '/admin/exams' },
+        { name: 'Job', icon: <FiBriefcase />, path: '/admin/jobs' },
+        { name: 'Report', icon: <FiFileText />, path: '/admin/reports' },
+        { name: 'Settings', icon: <FiSettings />, path: '/admin/settings' },
     ]
     return (
         <Sider collapsible collapsed={collapsed} onCollapse={() => setCollapsed(!collapsed)} >
